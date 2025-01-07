b52d0f8db4fc11e73fb6fa08676233acb663e19b
antonio.abreu <antonio.abreu@redeflex.com.br>
2025-01-07 17:45:54 -0400

    first commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GestaoPedidos.Models;
+
+namespace GestaoPedidos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+    }
+}
