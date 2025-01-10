58aec08a24fe2d3809ce230f39cc124c1d25c9d6
Acaj0 <antoniocarlosjr007@hotmail.com>
2025-01-09 20:17:12 -0400

    navbar e travas no front

--- obj/Debug/net5.0/GestãoPedidos.AssemblyInfo.cs
+++ obj/Debug/net5.0/GestãoPedidos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestãoPedidos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb89c11ae88025707b84a523cc2fa5c75c5cca10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa066a858fdd91e20c791ced56d14ed97f28a0a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestãoPedidos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestãoPedidos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
